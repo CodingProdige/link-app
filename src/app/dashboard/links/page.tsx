6e439ea3921726c515966f8e3b9529f215710aae
Dillon Jurgens <dillonjurgens@gmail.com>
2024-06-08 10:36:53 +0200

    User links profile page
    Show conditionally if userData && !authLoading

--- src/app/dashboard/links/page.tsx
+++ src/app/dashboard/links/page.tsx
@@ -44,28 +44,35 @@ export default function Links() {
   };
 
   return (
-    <div className={styles.linksPage}>
-      <div className={styles.linksContainer}>
-        <div className={styles.addLinkContainer}>
-          {
-            addLinkActive ? (
-              <AddLinkForm toggleAddLink={toggleAddLink} />
-            ) : (
-              <button className={styles.addLinkButton} onClick={toggleAddLink}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-                </svg>
-                Add Link
-              </button>
-            )
-          }
+    <>
+      {
+        userData && !authLoading && (
+          <div className={styles.linksPage}>
+          <div className={styles.linksContainer}>
+            <div className={styles.addLinkContainer}>
+              {
+                addLinkActive ? (
+                  <AddLinkForm toggleAddLink={toggleAddLink} />
+                ) : (
+                  <button className={styles.addLinkButton} onClick={toggleAddLink}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    </svg>
+                    Add Link
+                  </button>
+                )
+              }
+            </div>
+            {
+              links?.length > 0 && <DraggableList items={links} setItems={handleLinksUpdate} userId={user.uid} />
+            }
+          </div>
+    
+          {userData && <MobilePreview key={previewKey} linkPageUrl={linkPageUrl} />}
         </div>
-        {
-          links?.length > 0 && <DraggableList items={links} setItems={handleLinksUpdate} userId={user.uid} />
-        }
-      </div>
+        )
+      }
+    </>
 
-      {userData && <MobilePreview key={previewKey} linkPageUrl={linkPageUrl} />}
-    </div>
   );
 }
