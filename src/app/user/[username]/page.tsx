e566181526aa107d3dfbc175b5eb581281da61be
Dillon Jurgens <dillonjurgens@gmail.com>
2024-06-08 13:36:49 +0200

    testing basic public link implementation

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,87 +1,93 @@
-"use client";
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import { notFound } from 'next/navigation';
-import styles from '@/styles/userlinkPage.module.scss';
-import Loading from '@/components/Loading';
-import Link from 'next/link';
-import { fetchUserDataByUsername } from '@/utils/firebaseUtils';
-
-interface UserPageProps {
-  params: {
-    username: string;
-  };
+export default async function PublicLink({params: {username}}) {
+  return <div>{username}</div>;
 }
 
-const UserPage = ({ params: { username } }: UserPageProps) => {
-  const [userData, setUserData] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const data = await fetchUserDataByUsername(username);
 
-        if (!data) {
-          notFound();
-          return;
-        }
+// "use client";
+// import { useState, useEffect } from 'react';
+// import Image from 'next/image';
+// import { notFound } from 'next/navigation';
+// import styles from '@/styles/userlinkPage.module.scss';
+// import Loading from '@/components/Loading';
+// import Link from 'next/link';
+// import { fetchUserDataByUsername } from '@/utils/firebaseUtils';
 
-        setUserData(data);
-      } catch (err) {
-        console.error('Error fetching user data:', err);
-        setError('Failed to load user data.');
-        notFound();
-      } finally {
-        setLoading(false);
-      }
-    };
+// interface UserPageProps {
+//   params: {
+//     username: string;
+//   };
+// }
 
-    fetchUserData();
-  }, [username]);
+// const UserPage = ({ params: { username } }: UserPageProps) => {
+//   const [userData, setUserData] = useState<any>(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState<string | null>(null);
 
-  if (loading) return <Loading />;
+//   useEffect(() => {
+//     const fetchUserData = async () => {
+//       try {
+//         const data = await fetchUserDataByUsername(username);
 
-  if (error) {
-    return <div className={styles.error}>{error}</div>;
-  }
+//         if (!data) {
+//           notFound();
+//           return;
+//         }
 
-  if (!userData) {
-    return <div className={styles.error}>User data not found.</div>;
-  }
+//         setUserData(data);
+//       } catch (err) {
+//         console.error('Error fetching user data:', err);
+//         setError('Failed to load user data.');
+//         notFound();
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
 
-  return (
-    <div className={styles.containerPublicProfile}>
-      <div className={styles.background}></div>
-      <div className={styles.innerContainer}>
-        <div className={styles.profileContainer}>
-          {userData?.photoUrl && (
-            <Image 
-              src={userData.photoUrl} 
-              alt={userData.username} 
-              width={150} 
-              height={150} 
-            />
-          )}
-          <p className={styles.username}>@{userData.username}</p>
-        </div>
-        {userData?.links && (
-          <div className={styles.linksContainer}>
-            <ul>
-              {userData?.links.map((link: any) => (
-                <Link href={link.link} key={link.id} target="_blank" rel="noopener noreferrer">
-                  <li key={link.id}>
-                    {link.title}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+//     fetchUserData();
+//   }, [username]);
 
-export default UserPage;
+//   if (loading) return <Loading />;
+
+//   if (error) {
+//     return <div className={styles.error}>{error}</div>;
+//   }
+
+//   if (!userData) {
+//     return <div className={styles.error}>User data not found.</div>;
+//   }
+
+//   return (
+//     <div className={styles.containerPublicProfile}>
+//       <div className={styles.background}></div>
+//       <div className={styles.innerContainer}>
+//         <div className={styles.profileContainer}>
+//           {userData?.photoUrl && (
+//             <Image 
+//               src={userData.photoUrl} 
+//               alt={userData.username} 
+//               width={150} 
+//               height={150} 
+//             />
+//           )}
+//           <p className={styles.username}>@{userData.username}</p>
+//         </div>
+//         {userData?.links && (
+//           <div className={styles.linksContainer}>
+//             <ul>
+//               {userData?.links.map((link: any) => (
+//                 <Link href={link.link} key={link.id} target="_blank" rel="noopener noreferrer">
+//                   <li key={link.id}>
+//                     {link.title}
+//                   </li>
+//                 </Link>
+//               ))}
+//             </ul>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default UserPage;
