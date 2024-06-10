d13f439fb13c81e7ded15ae25b50ede8e6fb7fcb
Dillon Jurgens <dillonjurgens@gmail.com>
2024-06-10 07:40:17 +0200

    ENV variables updated

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -4,9 +4,9 @@ import admin from 'firebase-admin';
 if(!admin.apps.length){
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+      privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY!.replace(/\\n/g, '\n'),
     }),
   });
 }
